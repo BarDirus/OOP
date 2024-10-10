60df1d5ee16b23d3cf7a8b28004e175c6827b616
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-10-10 20:31:43 +0700

    1

--- Task_1_1_3/src/test/java/ru/nsu/shirokov/ComplexExpressionTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/shirokov/ComplexExpressionTest.java
@@ -1,12 +1,11 @@
 package ru.nsu.shirokov;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
+import org.junit.jupiter.api.Test;
 
 class ComplexExpressionTest {
 
