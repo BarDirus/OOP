aec11db1a5cb16bacfb4f751cf46fe47317d1b05
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-10-10 20:35:56 +0700

    1

--- Task_1_1_3/src/test/java/ru/nsu/shirokov/NumberTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/shirokov/NumberTest.java
@@ -1,6 +1,7 @@
 package ru.nsu.shirokov;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class NumberTest {
