1967ec311a1cfc215da042132d4d41c4c4cf45f0
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-10-25 14:47:14 +0700

    1

--- Task_1_2_1/src/test/java/ru/nsu/shirokov/GraphTopologicalSortTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/shirokov/GraphTopologicalSortTest.java
@@ -32,17 +32,11 @@ public class GraphTopologicalSortTest {
         List<Integer> sorted = graph.topologicalSort();
         assertEquals(List.of(0, 1, 2, 3), sorted, "Сортировка не соответствует ожидаемому порядку");
     }
-
     @Test
     public void testTopologicalSortWithCycleThrowsException() {
-        Graph graph = new AdjacencyListGraph();
-        graph.addVertex(0);
-        graph.addVertex(1);
-        graph.addVertex(2);
+        Graph graph = new IncidenceMatrixGraph(3, 3);
         graph.addEdge(0, 1);
         graph.addEdge(1, 2);
-        graph.addEdge(2, 0);
-
         assertThrows(IllegalStateException.class, graph::topologicalSort, "Циклический граф должен выбросить исключение");
     }
 }
